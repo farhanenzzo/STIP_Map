51fa7b0437d2014bf929b46f315d12a4438acdc5
onthego <farhan.enzo99@gmail.com>
2024-04-16 11:01:02 +0600

    spring-init-file

---
+++ src/main/java/SquareHealth/Map/Medicine_User/MedicineUserApplication.java
@@ -0,0 +1,13 @@
+package SquareHealth.Map.Medicine_User;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SquareHealth/Map/Medicine_User/MedicineUserApplicationTests.java
@@ -0,0 +1,13 @@
+package SquareHealth.Map.Medicine_User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
